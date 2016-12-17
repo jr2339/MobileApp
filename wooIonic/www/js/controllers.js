01e0021cd394591d1c12494586db971accc2c5c3
jr2339 <jr2339@nau.edu>
2016-12-16 20:28:45 -0500

    Adding Pull Down to Refresh and Infinite Scroll

--- wooIonic/www/js/controllers.js
+++ wooIonic/www/js/controllers.js
@@ -20,6 +20,10 @@ angular.module('starter.controllers',[])
 })
 
 .controller('BrowserCtrl',function ($scope,WC) {
+
+    $scope.offset = 0;
+
+
     $scope.getProducts = function () {
         var Woocommerce = WC.WC();
 
@@ -30,8 +34,40 @@ angular.module('starter.controllers',[])
             console.log(JSON.parse(res));
 
             $scope.products = JSON.parse(res).products;
+            $scope.offset = $scope.offset + 10;
+            $scope.canLoadMore = true;
         })
     }
 
     $scope.getProducts();
+
+    $scope.doRefresh = function () {
+        $scope.getProducts();
+        $scope.$broadcast('scroll.refreshComplete');
+
+    }
+    $scope.loadMore = function () {
+        var Woocommerce = WC.WC();
+
+        Woocommerce.get('products?filter[offset]='+$scope.offset,function (err,data,res) {
+            if(err)
+                console.log(err);
+
+            JSON.parse(res).products.forEach(function (element,index) {
+                $scope.products.push(element);
+            })
+
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+
+            if(JSON.parse(res).products.length < 10){
+                $scope.canLoadMore = false;
+                console.log("no more products!");
+                return;
+            }
+            else{
+              $scope.offset = $scope.offset + 10;
+            }
+
+        })
+    }
 })
